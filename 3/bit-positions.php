92499fc7f48b33d521a7404d677fd71dc58be16d
Dmitry Dzubenko <xdeenist@gmail.com>
2016-12-23 01:33:36 +0200

    initial commit

--- 3/bit-positions.php
+++ 3/bit-positions.php
@@ -5,7 +5,10 @@ while (false !== ($line = fgets($file))){
       list($num1, $num2, $num3) = explode(",", $line);
 
       $bits = decbin($num1);
-//      print_r($bits) ;
- //     echo "\n";
+            print_r($bits) ;
+            echo "\n";
+ //      $leftBit  = $bits[strlen($bits) - $num2];
+ //      print_r($leftBit);
+ //      echo "\n";
 }
 fclose($file);
---
+++ 4/18_codeval.php
@@ -0,0 +1,28 @@
+<?php
+//looping through file
+foreach (file($argv[1]) as $value) {
+    //creating or reseting variables
+    $result = [];
+    $rank = 1;
+    $ranks = [$rank];
+    //getting numbers to process
+    list($a, $b) = array_map('intval', explode(',', $value));
+    //defining ranks used in final multiplier
+    while($b * ($rank  10) < $a) {
+        $rank *= 10;
+        $ranks[] = $rank;
+    }
+    $ranks = array_reverse($ranks);
+    //defining exact multiplier for each rank
+    foreach ($ranks as $rank) {
+        $multiplier = isset($result[$rank]) ? $result[$rank] : 1;
+        while(($rank * ($multiplier + 1) * $b) < $a) {
+            $multiplier++;
+        }
+        $result[$rank] = $multiplier;
+        if($rank != 1) {
+            $result[$rank * 0.1] = $multiplier  10;
+        }
+    }
+    echo $b * ($result[1]+1) . "\n";
+}
