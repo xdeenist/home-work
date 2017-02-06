2f0092ca1e5fc493f8b9ef951f7b00ae08e50375
Dmitry Dzubenko <xdeenist@gmail.com>
2017-02-06 23:00:50 +0200

    fix

--- blog/include/function.php
+++ blog/include/function.php
@@ -8,7 +8,7 @@ function comment($row){ ?>
         <div style="margin-left: 30px">
 	        <div id="<?=$row['comment_id']?>" class="date"><?=$row['comment_username'] . " " . $row['comment_datetime'] ?></div>
 	        <div class="text"><?=$row['comment_text']?></div>
-	        <a href="#<?=$row['comment_parent_id']?>" class="glyphicon glyphicon-arrow-up"></a>
+	        <a href="#<?=$row['comment_parent_id']?>" class="glyphicon glyphicon-bell"></a>
 	        <a href="?id=<?=$_GET['id']?>&comment=<?=$row['comment_id']?>#answer">Ответить</a>
         <?php if ($_SESSION['logined']) { ?>
     	    | <a href='?id=<?=$_GET['id']?>&comment_del=<?=$row['comment_id']?>'>Удалить</a> 
