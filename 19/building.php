ebfb3f4caa39909fa70d993d2f70d1ed977e314e
Dmitry Dzubenko <xdeenist@gmail.com>
2017-02-12 01:03:35 +0200

    up

--- 19/building.php
+++ 19/building.php
@@ -10,8 +10,12 @@ class Home
 	public $kvartir_v_pod;
 
 	public function KakoyDom(){
-			if (condition) {
-				# code...
+			if ($this->tipDoma == "KHR_5_4") {
+				$this->kvartir_v_pod = 15;
+				$this->kvarvirNaEtag = 3;
+			} elseif ($this->tipDoma == "SLT_9_7") {
+				$this->kvartir_v_pod = 36;
+				$this->kvarvirNaEtag = 4;
 			}
 	}
 
@@ -27,13 +31,11 @@ class Home
 $KHR_5_4 = new Home;
 $KHR_5_4->tipDoma = "KHR_5_4";
 $KHR_5_4->number = 172;
-$KHR_5_4->kvartir_v_pod = 15;
-$KHR_5_4->kvarvirNaEtag = 3;
+$KHR_5_4->KakoyDom();
 $KHR_5_4->HomeEtaj();
 
 $SLT_9_7 = new Home;
 $SLT_9_7->tipDoma = "SLT_9_7";
 $SLT_9_7->number = 36;
-$SLT_9_7->kvartir_v_pod = 36;
-$SLT_9_7->kvarvirNaEtag = 4;
+$SLT_9_7->KakoyDom();
 $SLT_9_7->HomeEtaj();
